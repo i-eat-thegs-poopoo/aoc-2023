c341283acf335d662a0896178c00459f63477ff7
Hi Leo <stinkeyecookie@gmail.com>
2023-12-11 19:04:17 -0800

    Day 11

---
+++ day-11/src/main.rs
@@ -0,0 +1,69 @@
+fn main() {
+    let (one, two) = utils::setup();
+    one(|input| run(input, 2));
+    two(|input| run(input, 1_000_000));
+}
+
+pub fn run(input: &str, expand: u64) -> u64 {
+    let (galaxies, empty_rows, empty_cols) = parse(input);
+    let mut sum = 0;
+
+    for i in 0..galaxies.len() - 1 {
+        let curr = galaxies[i];
+
+        for &(ax, ay) in &galaxies[i + 1..] {
+            let (bx, by) = curr;
+
+            let mut calc_axis = |a, b, empty_axis: &[_]| {
+                let range = if a < b { a..b } else { b..a };
+                for n in range {
+                    sum += if empty_axis.contains(&n) { expand } else { 1 };
+                }
+            };
+
+            calc_axis(ax, bx, &empty_cols);
+            calc_axis(ay, by, &empty_rows);
+        }
+    }
+
+    sum
+}
+
+fn parse(input: &str) -> (Vec<(usize, usize)>, Vec<usize>, Vec<usize>) {
+    let mut galaxies = Vec::new();
+    let mut empty_rows = Vec::new();
+    let mut empty_cols = Vec::new();
+
+    for (y, line) in input.lines().enumerate() {
+        let mut row_is_empty = true;
+
+        for (x, char) in line.chars().enumerate() {
+            if empty_cols.len() <= x {
+                empty_cols.push(true);
+            }
+
+            match char {
+                '#' => {
+                    galaxies.push((x, y));
+                    row_is_empty = false;
+                    empty_cols[x] = false;
+                }
+                '.' => continue,
+                _ => panic!(),
+            }
+        }
+
+        if row_is_empty {
+            empty_rows.push(y);
+        }
+    }
+
+    let empty_cols = empty_cols
+        .into_iter()
+        .enumerate()
+        .filter(|(_, is_empty)| *is_empty)
+        .map(|(x, _)| x)
+        .collect::<Vec<_>>();
+
+    (galaxies, empty_rows, empty_cols)
+}
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{env, fs};
+use std::{env, fmt::Display, fs, rc::Rc, time};
 
 mod parse;
 
@@ -7,4 +7,27 @@ pub use parse::*;
 pub fn read_input() -> String {
     let path = env::args().nth(1).expect("Path not supplied");
     fs::read_to_string(path).expect("File not found")
+}
+
+pub fn run<Out: Display>(part: &str, func: impl FnOnce(&str) -> Out, input: &str) {
+    let now = time::Instant::now();
+    let out = func(input);
+    let dur = now.elapsed();
+
+    println!("Part {part}: {out}\n Elapsed: {dur:?}");
+}
+
+pub fn setup<
+    OneOut: Display,
+    TwoOut: Display,
+    One: FnOnce(&str) -> OneOut,
+    Two: FnOnce(&str) -> TwoOut,
+>() -> (impl FnOnce(One), impl FnOnce(Two)) {
+    let one_input = Rc::new(read_input());
+    let two_input = one_input.clone();
+
+    let one = move |func| run("one", func, one_input.as_str());
+    let two = move |func| run("two", func, two_input.as_str());
+
+    (one, two)
 }
