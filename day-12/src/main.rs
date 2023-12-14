e772154974fcc04dbcd13076a73ab9e996efa321
Hi Leo <stinkeyecookie@gmail.com>
2023-12-13 22:51:06 -0800

    Day 12 part 2

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -1,16 +1,34 @@
+use std::collections::HashMap;
+
 fn main() {
     let (one, two) = utils::setup();
     one(|input| {
-        parse(input)
-            .into_iter()
-            .map(|rec| find_unknowns(&rec.springs, &rec.groups))
-            .sum::<usize>()
-    });
+        let mut cache = HashMap::new();
 
-    two(|input| {
         parse(input)
             .into_iter()
             .map(|rec| {
+                cache.clear();
+
+                let state = State {
+                    next_spring: 0,
+                    next_group: 0,
+                    remaining: None,
+                };
+
+                find_combos(&rec.springs, &rec.groups, state, &mut cache)
+            })
+            .sum::<u64>()
+    });
+
+    two(|input| {
+        let mut cache = HashMap::new();
+
+        parse(input)
+            .into_iter()
+            .map(|rec| {
+                cache.clear();
+
                 let mut springs = rec.springs;
                 springs.push(Spring::Unknown);
                 let mut springs = springs.repeat(5);
@@ -18,9 +36,15 @@ fn main() {
 
                 let groups = rec.groups.repeat(5);
 
-                find_unknowns(&springs, &groups)
+                let state = State {
+                    next_spring: 0,
+                    next_group: 0,
+                    remaining: None,
+                };
+
+                find_combos(&springs, &groups, state, &mut cache)
             })
-            .sum::<usize>()
+            .sum::<u64>()
     });
 }
 
@@ -71,62 +95,77 @@ fn parse(input: &str) -> Vec<Record> {
     records
 }
 
+#[derive(Clone, PartialEq, Eq, Hash)]
 struct State {
     next_spring: usize,
     next_group: usize,
     remaining: Option<u64>,
 }
 
-fn find_unknowns(springs: &[Spring], groups: &[u64]) -> usize {
-    let mut combos = 0;
-    let mut state_stack = vec![State {
-        next_spring: 0,
-        next_group: 0,
-        remaining: None,
-    }];
-
-    'outer: while let Some(mut state) = state_stack.pop() {
-        for spring in state.next_spring..springs.len() {
-            state.remaining = match springs[spring] {
-                Spring::Operational => match state.remaining {
-                    Some(0) | None => None,
-                    Some(_) => continue 'outer,
-                },
-                Spring::Damaged => match state.remaining {
-                    Some(0) => continue 'outer,
-                    Some(len) => Some(len - 1),
-                    None => match groups.get(state.next_group) {
-                        Some(len) => {
-                            state.next_group += 1;
-                            Some(len - 1)
-                        }
-                        None => continue 'outer,
-                    },
-                },
-                Spring::Unknown => match state.remaining {
-                    Some(0) => None,
-                    Some(len) => Some(len - 1),
-                    None => {
-                        // Assume damaged
-                        if let Some(len) = groups.get(state.next_group) {
-                            state_stack.push(State {
-                                next_spring: spring + 1,
-                                next_group: state.next_group + 1,
-                                remaining: Some(len - 1),
-                            });
-                        }
-
-                        // Assume operational
-                        None
-                    }
-                },
-            };
-        }
-
-        if state.next_group == groups.len() && state.remaining.unwrap_or(0) == 0 {
-            combos += 1;
-        }
+fn find_combos(
+    springs: &[Spring],
+    groups: &[u64],
+    mut state: State,
+    cache: &mut HashMap<State, u64>,
+) -> u64 {
+    if let Some(combos) = cache.get(&state) {
+        return *combos;
     }
 
-    combos
+    while state.next_spring < springs.len() {
+        state.remaining = match springs[state.next_spring] {
+            Spring::Operational => match state.remaining {
+                Some(0) | None => None,
+                Some(_) => return 0,
+            },
+            Spring::Damaged => match state.remaining {
+                Some(0) => return 0,
+                Some(len) => Some(len - 1),
+                None => match groups.get(state.next_group) {
+                    Some(len) => {
+                        state.next_group += 1;
+                        Some(len - 1)
+                    }
+                    None => return 0,
+                },
+            },
+            Spring::Unknown => match state.remaining {
+                Some(0) => None,
+                Some(len) => Some(len - 1),
+                None => {
+                    let if_operational = {
+                        let state = State {
+                            next_spring: state.next_spring + 1,
+                            next_group: state.next_group,
+                            remaining: None,
+                        };
+
+                        find_combos(springs, groups, state, cache)
+                    };
+
+                    let if_damaged = groups
+                        .get(state.next_group)
+                        .map(|len| {
+                            let state = State {
+                                next_spring: state.next_spring + 1,
+                                next_group: state.next_group + 1,
+                                remaining: Some(len - 1),
+                            };
+
+                            find_combos(springs, groups, state, cache)
+                        })
+                        .unwrap_or(0);
+
+                    let combos = if_operational + if_damaged;
+                    cache.insert(state, combos);
+
+                    return combos;
+                }
+            },
+        };
+
+        state.next_spring += 1;
+    }
+
+    (state.next_group == groups.len() && state.remaining.unwrap_or(0) == 0) as u64
 }
